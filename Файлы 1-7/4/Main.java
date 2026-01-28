cb5539f5acefccef2b8cfcf2c084579240049f0d
bolshakovsm <83596908+bolshakovsm@users.noreply.github.com>
2026-01-29 00:38:39 +0300

    Add files via upload

---
+++ Файлы 1-7/4/Main.java
@@ -0,0 +1,17 @@
+// 4. Подсчитать количество строк в текстовом файле.
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try (FileReader fr = new FileReader("1.txt");
+         LineNumberReader lnr = new LineNumberReader(fr)) {
+      while (lnr.readLine() != null);
+      System.out.println(lnr.getLineNumber());      
+    } catch (FileNotFoundException e) {
+      System.out.println("FileNotFoundException " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("IOException " + e.getMessage());
+    }
+  }
+}
