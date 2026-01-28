ffe5b918aafb6eb2cfcc0c9de5614cfbaddb9d79
bolshakovsm <83596908+bolshakovsm@users.noreply.github.com>
2026-01-29 00:37:15 +0300

    Add files via upload

---
+++ Файлы 1-7/1/Main.java
@@ -0,0 +1,13 @@
+// 1. Создать текстовый файл и записать в него строку "Hello, File!".
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try (FileWriter fw = new FileWriter("1.txt")) {  // exception test "x://1.txt"
+      fw.write("Hello, File!");
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
