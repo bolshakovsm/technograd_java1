8839d101fbe36454efe033ae910a63f9e66c5b0f
bolshakovsm <83596908+bolshakovsm@users.noreply.github.com>
2026-01-29 00:37:39 +0300

    Add files via upload

---
+++ Файлы 1-7/2/Main.java
@@ -0,0 +1,16 @@
+// 2. Прочитать содержимое текстового файла и вывести его на экран.
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try (FileReader fr = new FileReader("1.txt");
+         BufferedReader br = new BufferedReader(fr)) {
+      System.out.println(br.readLine());      
+    } catch (FileNotFoundException e) {
+      System.out.println("FileNotFoundException " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("IOException " + e.getMessage());
+    }
+  }
+}
